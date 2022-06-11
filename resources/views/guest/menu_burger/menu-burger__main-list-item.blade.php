4cf1790b8499c8a2df2badec8d65a4f8a9f7d0c4
nvlight <lightstyle11@mail.ru>
2022-06-11 22:30:40 +0300

    simlpy rework blade file, now svg with and without hover for main burger menu works fine

--- resources/views/guest/menu_burger/menu-burger__main-list-item.blade.php
+++ resources/views/guest/menu_burger/menu-burger__main-list-item.blade.php
@@ -2,3 +2,13 @@
     <a href="https://www.ildberries.ru/catalog/---{{$category->id}}"
        class="menu-burger__main-list-link menu-burger__main-list-link--{{$category->id}}">{{$category->title}}</a>
 </li>
+<style>
+    @if($category->svg1)
+        .menu-burger__main-list-link.menu-burger__main-list-link--{{$category->id}}::before {
+            background-image: url({{asset(env('BURGER_MENU_1ST_LEVEL_SVGS_SHOW_PATH').$category->svg1)}});
+        }
+        .menu-burger__main-list-item--active .menu-burger__main-list-link.menu-burger__main-list-link--{{$category->id}}::before {
+            background-image: url({{asset(env('BURGER_MENU_1ST_LEVEL_SVGS_SHOW_PATH').$category->svg2)}});
+        }
+    @endif
+</style>
