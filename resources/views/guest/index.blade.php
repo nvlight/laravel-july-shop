20c8c9315e3eb45a91db9dab8087e85f9452e3a6
nvlight <lightstyle11@mail.ru>
2022-06-13 15:06:08 +0300

    first part of footer design is done

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -5,14 +5,9 @@
 @section('content')
     @include('guest.parts.header')
     @include('guest.parts.nav')
-    <nav>
-        Nav (display: none) - after press burger button (display: block)
-    </nav>
     <main class="main">
     </main>
-    <footer>
-        Footer
-    </footer>
+    @include('guest.parts.footer')
 @endsection
 
 @section('nav_mobile')
---
+++ resources/views/guest/parts/footer.blade.php
@@ -0,0 +1,112 @@
+<footer class="footer j-footer footer-ru" id="footer" data-link="
+        {on 'click' '.j-dropdown-title' ~root.onMenuSelected}
+        {on 'click' '.j-wba-footer-item' ~root.sendStatEvent}" data-jsv="#11^/11^#12^/12^">
+    <div class="footer__container">
+        <div class="footer__nav">
+            <div class="footer__service-wrap j-dropdown-footer">
+                <div class="footer__list-wrap footer__list-wrap--buyers">
+                    <div class="footer__header-wrap j-dropdown-title">
+                        <h3 class="footer__header">Покупателям</h3>
+                        <button class="footer__btn-open" type="button">Открыть</button>
+                    </div>
+                    <ul class="footer__list footer__list--dropdown">
+                        <li class="footer__item j-wba-footer-item" data-wba-footer-name="How_to_order"><a rel="nofollow" href="/services/kak-sdelat-zakaz">Как сделать заказ</a></li>
+                        <li class="footer__item j-wba-footer-item" data-wba-footer-name="Pay_methods"><a rel="nofollow" href="/services/sposoby-oplaty">Способы оплаты</a></li>
+                        <li class="footer__item j-wba-footer-item" data-wba-footer-name="Delivery"><a rel="nofollow" href="/services/besplatnaya-dostavka">Доставка</a></li>
+                        <li class="footer__item j-wba-footer-item" data-wba-footer-name="Return_order"><a rel="nofollow" href="/services/vozvrat-tovara">Возврат товара</a></li>
+                        <li class="footer__item j-wba-footer-item" data-wba-footer-name="Refunds"><a rel="nofollow" href="/services/vozvrat-denezhnyh-sredstv">Возврат денежных средств</a></li>
+                        <li class="footer__item j-wba-footer-item" data-wba-footer-name="Sale_policy"><a rel="nofollow" href="/services/pravila-prodazhi">Правила продажи</a></li>
+                        <li class="footer__item j-wba-footer-item" data-wba-footer-name="Store_policy"><a rel="nofollow" href="/services/pravila-polzovaniya-torgovoy-ploshchadkoy" data-link="text{:~wbSettings.currentLocale == 'ru' || ~wbSettings.currentLocale == 'by' ? 'Правила пользования торговой площадкой' : 'Публичная оферта'}">Правила пользования торговой площадкой</a></li>
+                        <li class="footer__item j-wba-footer-item" data-wba-footer-name="Questions"><a rel="nofollow" href="/services/voprosy-i-otvety">Вопросы и ответы</a></li>
+                    </ul>
+                </div>
+
+                <div class="footer__inner-wrap">
+                    <div class="footer__list-wrap">
+                        <div class="footer__header-wrap j-dropdown-title">
+                            <h3 class="footer__header">Партнерам</h3>
+                            <button class="footer__btn-open" type="button">Открыть</button>
+                        </div>
+                        <ul class="footer__list footer__list--dropdown">
+                            <li class="footer__item j-wba-footer-item" data-wba-footer-name="Seller"><a rel="nofollow" target="_blank" href="https://seller.wildberries.ru/">Продавайте на Wildberries</a></li>
+
+                            <li class="footer__item j-wba-footer-item" data-wba-footer-name="Carrier"><a rel="nofollow" target="_blank" href="/promo/priglashaem-k-sotrudnichestvu">Перевозчикам</a></li>
+
+                            <li class="footer__item j-wba-footer-item" data-wba-footer-name="Pick_up_points_open"><a rel="nofollow" target="_blank" href="https://point-promo.wb.ru/">Откройте пункт выдачи</a></li>
+
+                            <li class="footer__item j-wba-footer-item" data-wba-footer-name="Franchise"><a rel="nofollow" target="_blank" href="/services/franshizniy-punkt-vydachi">Франшизный пункт выдачи</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="footer__list-wrap">
+                        <div class="footer__header-wrap j-dropdown-title">
+                            <h3 class="footer__header">Наши проекты</h3>
+                            <button class="footer__btn-open" type="button">Открыть</button>
+                        </div>
+                        <ul class="footer__list footer__list--dropdown">
+                            <li class="footer__item j-wba-footer-item" data-wba-footer-name="Guru"><a rel="nofollow" target="_blank" href="https://guru.wildberries.ru/?utm_source=main_footer">WB Guru</a></li>
+                            <li class="footer__item j-wba-footer-item" data-wba-footer-name="Job"><a rel="nofollow" target="_blank" href="https://vsemrabota.ru/appwb/">Трудоустройство</a></li>
+                            <li class="footer__item j-wba-footer-item" data-wba-footer-name="Digital"><a rel="nofollow" target="_blank" href="https://digital.wildberries.ru/">Цифровые товары</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="footer__list-wrap footer__list-wrap--company">
+                    <div class="footer__header-wrap j-dropdown-title">
+                        <h3 class="footer__header">Компания</h3>
+                        <button class="footer__btn-open" type="button">Открыть</button>
+                    </div>
+                    <ul class="footer__list footer__list--dropdown">
+                        <li class="footer__item j-wba-footer-item" data-wba-footer-name="About_us"><a rel="nofollow" href="/services/o-nas">О нас</a></li>
+                        <li class="footer__item j-wba-footer-item" data-wba-footer-name="Bank_info"><a rel="nofollow" href="/services/rekvizity">Реквизиты</a></li>
+                        <li class="footer__item j-wba-footer-item" data-wba-footer-name="Press"><a rel="nofollow" href="/presscenter">Пресс-центр</a></li>
+                        <li class="footer__item j-wba-footer-item" data-wba-footer-name="Сontacts"><a rel="nofollow" href="/services/kontakty">Контакты</a></li>
+                        <li class="footer__item j-wba-footer-item" data-wba-footer-name="Bug_Bounty"><a rel="nofollow" href="/services/bug-bounty">Bug Bounty</a></li>
+                        <li class="footer__item j-wba-footer-item" data-wba-footer-name="WB_Tech"><a target="_blank" rel="nofollow" href="https://t.me/wbdevs">WB.Tech</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="footer__social-wrap">
+                <div class="footer__list-wrap footer__list-wrap--download">
+                    <h3 class="footer__header">Мобильные устройства</h3>
+                    <ul class="footer__list">
+                        <li class="footer__item j-wba-footer-item" data-wba-footer-name="GooglePlay"><a class="google-play" rel="nofollow noopener" target="_blank" href="https://play.google.com/store/apps/details?id=com.wildberries.ru">Загрузить из Google Play</a></li>
+                        <li class="footer__item j-wba-footer-item" data-wba-footer-name="AppStore"><a class="app-store" rel="nofollow noopener" target="_blank" href="https://apps.apple.com/ru/app/wildberries/id597880187">Загрузить из AppStore</a></li>
+                        <li class="footer__item j-wba-footer-item" data-wba-footer-name="AppGallery"><a class="app-gallery" rel="nofollow noopener" target="_blank" href="https://appgallery8.huawei.com/#/app/C101183325">Загрузить из AppGallery</a></li>
+                    </ul>
+                </div>
+
+                <div class="footer__list-wrap footer__list-wrap--social">
+                    <h3 class="footer__header">Мы в соцсетях</h3>
+                    <ul class="footer__list">
+                        <li class="footer__item j-wba-footer-item" data-wba-footer-name="Vk"><a class="icon-vk" rel="nofollow noopener" target="_blank" href="http://vk.com/club9695053">Вконтакте</a></li>
+
+                        <li class="footer__item j-wba-footer-item" data-wba-footer-name="Ok"><a class="icon-ok" rel="nofollow noopener" target="_blank" href="http://www.odnoklassniki.ru/wildberries">Одноклассники</a></li>
+                        <li class="footer__item j-wba-footer-item" data-wba-footer-name="YouTube"><a class="icon-yt" rel="nofollow noopener" target="_blank" href="https://www.youtube.com/Wildberriesshop">YouTube</a></li>
+                        <li class="footer__item"><a class="icon-tg" rel="nofollow noopener" target="_blank" href="https://t.me/wildberriesru_official">Телеграм</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer__add-info">
+            <ul class="footer__pay-list pay-list">
+                <li class="pay-list__item pay-list__item--visa"></li>
+                <li class="pay-list__item pay-list__item--masterCard"></li>
+                <li class="pay-list__item pay-list__item--mir"></li>
+            </ul>
+
+            <div class="footer__compatability">
+                <a rel="nofollow" class="j-wba-footer-item" data-wba-footer-name="Сompatibility" href="/services/proverka-sovmestimosti">Проверка совместимости</a>
+            </div>
+
+            <p class="footer__copyrights">
+
+
+                <span>2004-2022 © Wildberries</span><span class="footer__text-desktop"> — модный интернет-магазин одежды, обуви и аксессуаров. Все права защищены.</span>
+                <span class="footer__text-desktop">Доставка по всей России.</span>
+            </p>
+        </div>
+    </div>
+</footer>
