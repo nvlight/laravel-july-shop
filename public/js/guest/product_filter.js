8dba52f2e5ca546b4e23581f89eeac046e11822e
nvlight <lightstyle11@mail.ru>
2022-06-29 13:33:24 +0300

    filter type checkbox. chech/uncheck/reset is done

--- public/js/guest/product_filter.js
+++ public/js/guest/product_filter.js
@@ -1,4 +1,6 @@
 let filterShowHideList = ['.render_type_7', '.render_type_1', '.render_type_5', '.render_type_6'];
+let filterCheckboxClass = filterShowHideList[0];
+let labelClass = '.filter__item';
 
 /**
  * Для всех фильтров-выпадашек сделать скрытие/показ
@@ -13,13 +15,16 @@ function filtersShowHide() {
     for(let i=0; i<filter.length; i++){
         filter[i].addEventListener('click', function (e) {
             //console.log(e.target);
+            //console.log(e.target.parentElement);
             const filterClassName = 'filter__btn-reset';
-            const target = e.target.parentElement;
-            if (target.tagName === 'BUTTON' && target.classList.contains(filterClassName)) {
-                console.log('filter__btn-reset clicked!');
+            if (e.target.tagName === 'BUTTON' && e.target.classList.contains(filterClassName)) {
+                // console.log('filter__btn-reset clicked!');
+                // delete all selected for closest j-filter-container .selected
+                removeSelectedForClosestCheckBox(e.target);
                 return;
             }
 
+            const target = e.target.parentElement;
             // j-filter-container filter filterblock render_type_7 fdlvr show filter-active
             const filterContainer = target.closest('.j-filter-container');
             if (!filterContainer){
@@ -37,5 +42,71 @@ function filtersShowHide() {
     }
 }
 
+/**
+ * Обработчик нажатия на все фильтры-чекбоксы.
+ */
+function filterCheckbox() {
+    let concatClasses = filterCheckboxClass + ' ' + labelClass;
+    //console.log(concatClasses);
+
+    const vv = document.querySelectorAll(concatClasses);
+    for (let i=0; i<vv.length; i++) {
+        vv[i].addEventListener('click', function(e){
+            //console.log('label clicked!');
+
+            // find closest
+            let closestCont = e.target.closest('.j-filter-container');
+            if (!closestCont){
+                return;
+            }
+            // check selected item for them
+            const selClass = 'selected'
+
+            // remove selected if exists
+            let isSelected = closestCont.querySelector('.'+selClass);
+            if (isSelected){
+                isSelected.classList.remove(selClass);
+            }
+            // add selected class for current item
+            // first go to closest label
+            let parLabel = e.target;
+            //console.log(parLabel.tagName)
+            if (parLabel.tagName !== 'LABEL'){
+                let fLabel = parLabel.closest('label');
+                if (fLabel){
+                    parLabel = fLabel;
+                }
+            }
+            if ( !parLabel.classList.contains(selClass)){
+                parLabel.classList.add(selClass);
+            }
+
+        })
+    }
+}
+
+/**
+ * Если нажали на сброс фильтра с чекбоксами, найти сам чекбокс и удалить все активный чекбокс с селектом.
+ * @param target
+ */
+function removeSelectedForClosestCheckBox(target) {
+    const tg = target.closest('.j-filter-container')
+    if (!tg){
+        return;
+    }
+    const tgClass = 'selected';
+    const sels = tg.querySelectorAll('.'+tgClass);
+    console.log(sels)
+    if (!sels.length){
+        return;
+    }
+    for(let i=0; i<sels.length; i++){
+        if ( sels[i].classList.contains(tgClass)){
+            sels[i].classList.remove(tgClass);
+        }
+    }
+}
+
 ////////////////////////////
 filtersShowHide();
+filterCheckbox();
