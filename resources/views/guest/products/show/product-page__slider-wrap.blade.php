b4063c62e4e12e42daa05ba0285699c22c0d8591
nvlight <lightstyle11@mail.ru>
2022-07-18 12:06:52 +0300

    refactor product page. up/down arrow showed if images count >= 6

--- resources/views/guest/products/show/product-page__slider-wrap.blade.php
+++ resources/views/guest/products/show/product-page__slider-wrap.blade.php
@@ -30,20 +30,18 @@
 </script>
 
 <div class="product-page__slider-wrap">
-    <div class="product-page__sticky-wrap"
-         data-link="{include imageGalleryModel tmpl=imageGalleryModel.template}">
-        <script type="jsv#17623_"></script>
+    <div class="product-page__sticky-wrap">
         <div class="product-page__slider" id="c9b886c8-f0ab-8c86-91cd-ac1f72dc8e1b">
             <div class="sw-slider-kt-mix">
-                <p class="badge-express-sm hide"
-                   data-link="class{merge:!(enabledForRegion &amp;&amp; deliveryModel^deliveryInfoModel &amp;&amp; deliveryModel^deliveryInfoModel.isExpress) toggle='hide'}">
-                    Экспресс-доставка</p>
-                <p class="badge-abroad-sm hide"
-                   data-link="class{merge: !isImport toggle='hide'}{tooltip tmplName='abroadInfoTooltip' classes='i-tooltip-abroad-delivery' distance=4 pos='alignleft bottom'}"
-                   data-jsv="#5426^/5426^">Доставка из-за рубежа</p>
-                <button class="swiper-button-prev" type="button" tabindex="-1" role="button"
-                        aria-label="Previous slide" aria-disabled="true">Предыдущий слайд
-                </button>
+                <p class="badge-express-sm hide">Экспресс-доставка</p>
+                <p class="badge-abroad-sm hide">Доставка из-за рубежа</p>
+                
+                @if($sliderImages && count($sliderImages) >= 5)
+                    <button class="swiper-button-prev" type="button" tabindex="-1" role="button"
+                            aria-label="Previous slide" aria-disabled="true">Предыдущий слайд
+                    </button>
+                @endif
+
                 <div class="sw-slider-kt-mix__wrap">
                     <div class="swiper-container j-sw-images-carousel swiper-container-initialized swiper-container-vertical">
                         <ul class="swiper-wrapper" style="transform: translate3d(0px, 0px, 0px);">
@@ -64,12 +62,18 @@
                            href="/recommendation/catalog?type=visuallysimilar&amp;forproduct=9414496&amp;excludeNms=true">Похожие</a>
                     </div>
                 </div>
-                <button class="swiper-button-next" type="button" tabindex="0" role="button"
-                        aria-label="Next slide" aria-disabled="false">Следующий слайд
-                </button>
-                <p class="swiper-pagination j-pagination swiper-pagination-fraction"><span
-                        class="swiper-pagination-current">1</span>/<span
-                        class="swiper-pagination-total">11</span></p></div>
+
+                @if($sliderImages && count($sliderImages) >= 5)
+                    <button class="swiper-button-next" type="button" tabindex="0" role="button"
+                            aria-label="Next slide" aria-disabled="false">Следующий слайд
+                    </button>
+                @endif
+
+                <p class="swiper-pagination j-pagination swiper-pagination-fraction">
+                    <span class="swiper-pagination-current">1</span>/<span
+                        class="swiper-pagination-total">11</span>
+                </p>
+            </div>
         </div>
 
         <div class="product-page__mix-block hide-mobile">
